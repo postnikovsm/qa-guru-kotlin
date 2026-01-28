4b129db97dd7a14c8df0e3b4005965c9894a6d7d
postnikovsm <postnikov.simon@gmail.com>
2026-01-28 13:26:45 +0300

    first tests

---
+++ src/test/kotlin/QAGuruKotlinCoursePagePresenceTest.kt
@@ -0,0 +1,34 @@
+
+import io.qameta.allure.Feature
+import org.junit.jupiter.api.DisplayName
+import com.codeborne.selenide.Selenide.element
+import com.codeborne.selenide.Selenide.open
+import com.codeborne.selenide.Condition.text
+import com.codeborne.selenide.Condition.exactText
+import com.codeborne.selenide.Configuration
+import org.junit.jupiter.api.Test
+
+@Feature("Calculator Operations")
+class QAGuruKotlinCoursePagePresenceTest {
+
+    @Test
+    @DisplayName("Проверка названия курса")
+    fun checkNameCourse() {
+        Configuration.pageLoadStrategy = "eager";
+        open("https://qa.guru/kotlin")
+
+        element(".uui-heading-medium-curs .text-span")
+            .shouldHave(text("Kotlin"))
+    }
+
+    @Test
+    @DisplayName("Проверка стоимости курса")
+    fun checkPriceCourseTest() {
+        Configuration.pageLoadStrategy = "eager";
+        val price = "6 085"
+        open("https://qa.guru/kotlin")
+
+        element(".new-rassrochka-text-wa-course")
+            .shouldHave(exactText("Рассрочка от $price р/мес"))
+    }
+}
