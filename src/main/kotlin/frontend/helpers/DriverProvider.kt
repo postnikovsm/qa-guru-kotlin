141d9b7ac0e3bb733cdbc839285be43322c8c345
postnikovsm <postnikov.simon@gmail.com>
2026-02-27 22:25:25 +0300

    added moon

--- src/main/kotlin/frontend/helpers/DriverProvider.kt
+++ src/main/kotlin/frontend/helpers/DriverProvider.kt
@@ -8,23 +8,30 @@ import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URI
 
 open class DriverProvider : WebDriverProvider {
+    private val BROWSER_NAME = System.getProperty("browser", "chrome")
+
     override fun createDriver(capabilities: Capabilities): RemoteWebDriver {
-        return ChromeOptions().apply {
-            setCapability("browserName", "chrome")
-            setCapability("browserVersion", "latest")
-            setCapability(
-                "selenoid:options",
-                mapOf(
-                    "enableVNC" to true,
-                    "enableVideo" to false,
-                    "sessionTimeout" to "15m",
-                    "screenResolution" to "1920x1080"
-                )
-            )
-        }.run {
-            RemoteWebDriver(URI("http://localhost:5555/wd/hub").toURL(), this)
-        }.apply {
-            fileDetector = LocalFileDetector()
+        return when (BROWSER_NAME) {
+            "chrome" -> {
+                ChromeOptions().apply {
+                    setCapability("browserVersion", "latest")
+                    setCapability(
+                        "selenoid:options",
+                        mapOf(
+                            "headless" to false,
+                            "enableVNC" to true,
+                            "acceptInsecureCerts" to true,
+                            "screenResolution" to "1920x1080"
+                        )
+                    )
+                    addArguments("--disable-gpu")
+                    addArguments("window-size=1920,1080")
+                }
+            }
+
+            else -> throw Error("Browser is not defined")
         }
+            .run { RemoteWebDriver(URI("http://localhost:5555/wd/hub").toURL(), this) }
+            .apply { this.fileDetector = LocalFileDetector() }
     }
 }
